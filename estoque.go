5a09625c05d97433aa99493985c8cf15516ee461
Gabriel-Back-End <gabriel15fariaint@gmail.com>
2026-04-10 14:25:25 -0300

    Add files via upload

---
+++ estoque.go
@@ -0,0 +1,50 @@
+package main
+
+
+import "fmt"
+
+
+func main() {
+    
+	estoque := map[string] int {"Espada" : 50, "Escudo" : 80, "Poção" : 20}
+
+	
+	
+	var nome string
+
+
+	fmt.Println("Digite o Nome do Produto: ")
+	
+	fmt.Scan(&nome)
+	
+
+
+	resposta := veridicar_prod(nome, estoque)
+	
+	
+	fmt.Println(resposta)
+	
+
+}
+
+
+
+func veridicar_prod (nome string, lista map[string]int) string {
+
+
+	preco, ok := lista[nome]
+    
+    
+
+	if ok {
+        
+		return fmt.Sprintf("O produto %v está custando R$ %v,00 Reais", nome,preco)
+    
+	}else{
+        
+		return"não existe o Produto no estoque"
+    
+	}
+
+
+}
