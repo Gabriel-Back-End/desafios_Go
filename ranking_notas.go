ed3f1d779249c1350b7c02c5737fb48a78878974
Gabriel-Back-End <gabriel15fariaint@gmail.com>
2026-04-23 14:57:52 -0300

    adicionando o uso de funções, para o código ser melhor entendido

--- ranking_notas.go
+++ ranking_notas.go
@@ -1,6 +1,9 @@
 package main
-import "fmt"
-import "sort"
+import (
+    "fmt"
+    "sort"
+    "strings"
+)
 
 type aluno struct {
     nome string
@@ -9,8 +12,7 @@ type aluno struct {
 
 func main() {
     var turma []aluno
-    var notaTotalTurma float64
-    var mediaFinal float64
+    var resposta string
     
     fmt.Println("Olá Professor, vamos la?")
     
@@ -26,9 +28,18 @@ func main() {
             break
         }
         
-        fmt.Println("Digite a nota do aluno: ")
-        fmt.Scan(&novaNota)
-        
+        for{
+            fmt.Println("Digite a nota do aluno: (0 a 10)")
+            fmt.Scan(&novaNota)
+            
+            if novaNota >= 0 && novaNota <= 10 {
+                break
+            }else{
+                fmt.Println("Erro !!!\n")
+                fmt.Println("A nota tem que ser de 0 a 10\n")
+            }
+            
+        }
         novoAluno := aluno{nome: novoNome, nota: novaNota}   
         
         turma = append(turma, novoAluno)
@@ -38,15 +49,44 @@ func main() {
         return turma[i].nota > turma[j].nota
     })
     
-    for i, aluno:= range turma{
-        fmt.Printf("%dº lugar: %s - Nota: %.1f\n", i+1, aluno.nome, aluno.nota)
-        notaTotalTurma += aluno.nota
-    }
     if len(turma) > 0 {
-        mediaFinal = notaTotalTurma / float64(len(turma))
         
-        fmt.Printf("E a Média Total da turma foi de %.1f",mediaFinal)
+        for{
+            fmt.Println("Você deseja ver o Ranking das notas? (s/n)")
+            fmt.Scan(&resposta)
+            
+            resposta = strings.ToLower(resposta)
+            
+            if resposta == "s" {
+                imprimirRanking(turma)
+                break
+            }else if resposta == "n"{
+                break
+            }else{
+                fmt.Println("Digite apenas s ou n")
+            }
+        }
+        
+        media := calcularMedia(turma)
+        fmt.Printf("\nA Média Total da Turma foi de %.1f",media)
     }else{
         fmt.Println("Nenhum aluno/nota encontrado")
+    }
+}
+
+func calcularMedia (lista []aluno) float64{
+    var notaTotalTurma float64
+    
+    for _, aluno := range lista {
+        notaTotalTurma += aluno.nota
+    }
+    
+    return notaTotalTurma / float64(len(lista))
+}
+
+func imprimirRanking (lista []aluno) {
+    fmt.Println("\n--- RANKING OFICIAL ---")
+    for i, aluno:= range lista{
+        fmt.Printf("%dº lugar: %s - Nota: %.1f\n", i+1, aluno.nome, aluno.nota)
     }
 }
